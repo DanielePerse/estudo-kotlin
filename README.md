16ebe5b4cd13595efe36214a23d8b75b6851d43f
Daniele Mendes <daniele.mendes@Alice-001890.local>
2022-02-04 19:31:46 -0300

    adicionei readme e projeto campo minado

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# estudo-kotlin
+# Estudo Kotlin :rocket:
+
+Evolução no estudo de Kotlin.
+
+:heavy_check_mark: Conceitos Básicos;
+
+:heavy_check_mark: Funções
+
+:heavy_check_mark: Classes e Objetos
+
+:heavy_check_mark: Lambdas
+
+:heavy_check_mark: Coleções
+
+:heavy_check_mark: Projeto Campo Minado
---
+++ src/main/kotlin/projetoCampoMinado/visao/TelaPrincipal.kt
@@ -0,0 +1,42 @@
+package projetoCampoMinado.visao
+
+import projetoCampoMinado.modelo.Tabuleiro
+import projetoCampoMinado.modelo.TabuleiroEvento
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+
+fun main(args: Array<String>) {
+    TelaPrincipal()
+}
+
+class TelaPrincipal : JFrame() {
+    private val tabuleiro = Tabuleiro(qtdLinhas = 16, qtdColunas = 30, qtdMinas = 50)
+    private val painelTabuleiro = PainelTabuleiro(tabuleiro)
+
+    init {
+        tabuleiro.onEvento(this::mostrarResultado)
+        add(painelTabuleiro)
+
+        setSize(690, 438)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Campo minado"
+        isVisible = true
+    }
+
+    private fun mostrarResultado(evento: TabuleiroEvento) {
+        SwingUtilities.invokeLater {
+            val msg = when(evento) {
+                TabuleiroEvento.VITORIA -> "Voce ganhou!"
+                TabuleiroEvento.DERROTA -> "Voce perdeu... :P"
+            }
+
+            JOptionPane.showMessageDialog(this, msg)
+            tabuleiro.reiniciar()
+
+            painelTabuleiro.repaint()
+            painelTabuleiro.validate()
+        }
+    }
+}
